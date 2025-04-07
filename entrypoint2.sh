551f72bd119a4ab891cefdbf4aacd0edc896f1a0
Deepanshu Goel <f20241016@pilani.bits-pilani.ac.in>
2025-04-07 22:55:35 +0530

    deployment

--- entrypoint2.sh
+++ entrypoint2.sh
@@ -1,7 +1,16 @@
-#!/bin/sh
-#python manage.py collectstatic --no-input
+set -e  
 
-python manage.py makemigrations
-#python manage.py collectstatic --no-input
-python manage.py migrate
-exec "$@"
+echo "Waiting for PostgreSQL..."
+while ! nc -z $DB_HOST $DB_PORT; do
+  sleep 0.1
+done
+echo "PostgreSQL started"
+
+echo "Running database migrations..."
+python manage.py migrate --noinput
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear
+
+echo "Starting Gunicorn..."
+exec gunicorn --bind 0.0.0.0:8000 --workers 3 mysite.wsgi:application
